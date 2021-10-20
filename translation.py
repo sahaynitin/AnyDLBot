6cbab5e7d0d985f9cf714b7ee5677d2b53c8fdbd
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-20 10:26:10 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,87 +1,87 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 class Translation(object):
-    START_TEXT = """Hi. 🤠 Thank You for using me .\nIm a simple Telegram All-In-One Bot."""
-    RENAME_403_ERR = "<b>Sorry. You Are Not Permitted To Rename This File.</b>"
-    ABS_TEXT = " <b>Please Don't Be Selfish.</b>"
-    UPGRADE_TEXT = "<b>This Bot Is Free To Use If U R My  Friend.</b>"
-    FORMAT_SELECTION = "<b>Select The Desired Format:</b> "
-    SET_CUSTOM_USERNAME_PASSWORD = """<b>If You Want To Download Premium Videos, Provide In The Following Format:
-URL | filename | username | password</b>"""
-    NOYES_URL = "<b>This Is Dam Slow Link Bro! I Wont Waste My Time On This. Get Me A Fast Link</b>"
-    DOWNLOAD_START = "<b>Downloading Ur Files 🔻</b>"
-    UPLOAD_START = "<b>Uploading Ur Files 🔺</b>"
-    RCHD_BOT_API_LIMIT ="<b>Size Greater Than Maximum Allowed Size. Neverthless, Trying To Upload.</b>"
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\n<b>Sorry. But, I Cannot Upload Files Greater Than 2GB Due To Telegram API Limitations.</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>File Uploaded Successfully</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nUploaded in {} seconds."
-    NOT_AUTH_USER_TEXT = "U R Not Authorise To Do This. This Is Only <b>Admin</b> Command"
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription."
-    SAVED_CUSTOM_THUMB_NAIL = "<b>Custom video / file thumbnail saved. This image will be used in the video / file.</b>"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ <b>Custom thumbnail cleared succesfully."
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media Cleared Succesfully.</b>"
-    SAVED_RECVD_DOC_FILE = "<b>Document Downloaded Successfully.</b>"
-    CUSTOM_CAPTION_UL_FILE = " <b>Bot Created By \n   👉 @HxBots</b>"
-    NO_CUSTOM_THUMB_NAIL_FOUND = "<b>No Custom ThumbNail Found.</b>"
-    NO_VOID_FORMAT_FOUND = "Can You Check The Url? <b>I Am Unable To Detect Video Format From UrL.</b> If You Think This Could Be A Bug Please Report On https://t.me/HxSupport"
-    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    CURENT_PLAN_DETAILS = """Current plan details
---------
-Telegram ID: <code>{}</code>
-Plan name: 75GB Per Month
-Expires on: 10/12/2021
---------"""
-    HELP_USER = """<b>There Are Multiple Things I Can Do:</b>\n\n <b>All Supported Video Formats of https://rg3.github.io/youtube-dl/supportedsites.html</b>
 
- <b>Upload as file from any HTTP link</b>
+    START_TEXT = """
+Hey {}
 
- <b>Convert To Streamable Video, any Telegram media.\nReply /converttovideo to Any Doc File</b>
+I am Extreme Uploader Bot.\n 
+Just send me the url to get started !\n
+You can Use me to Download any link Except Drm Protected link 🔗\n
+Made With 💕 By @Tellybots_4u\n
+"""
+    HELP_TEXT = """
+<b>Link to Media or File</b>
+- Send a link for upload to telegram file or media.
 
- <b>ReName Telegram files, with custom thumbnail support.\nReply /rename To File</b>
+<b>Set Thumbnail</b>
+- Send a photo to make it as permanent thumbnail.
 
- <b>Get High Speed Direct Download Link Of Any Telegram File.\nReply /getlink To File</b>
---------
+<b>Deleting Thumbnail</b>
+- Send /deletethumbnail to deleting thumbnail.
 
-Send /me To Know Your Current Plan Details"""
-    REPLY_TO_DOC_GET_LINK = "<b>Reply to a Telegram media to get High Speed Direct Download Link.</b>"
-    REPLY_TO_DOC_FOR_C2V = "<b>Reply to a Telegram Media To Convert.</b>"
-    REPLY_TO_DOC_FOR_SCSS = "<b>Reply to a Telegram Media To Get Screenshots.</b>"
-    SOURCE = """<b>Hi!,</b>
+<b>Show Thumbnail</b>
+- Send /showthumb to view custom thumbnail.
 
-<b>○ My Name : <i>Hx AnyDLBot</i></b>
-<b>○ Creator : <a href='https://telegram.dog/Kirodewal'>This Person</a></b>
-<b>○ Credits : Everyone In This Journey</b>
-<b>○ Language : Python 3.9</b>
-<b>○ Library : Pyrogram Asyncio</b>
-<b>○ Cloned From : AnyDLBot [Spechide]</b>
-<b>○ Source Code :  <a href='https://t.me/HxSourceCode/2'>👉 Click Here</a></b>
-<b>○ Server : <a href='aws.amazon.com'>AWS Amazon</a></b>
-<b>○ Build Status : Beta v1</b>"""
-    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Reply to a Telegram Media To /rename With Custom Thumbnail Support.</b>"
-    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days."
-    FF_MPEG_RO_BOT_RE_SURRECT_ED = """Syntax: /trim HH:MM:SS [HH:MM:SS]"""
-    FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "<b>First send /downloadmedia To Any Media So That It Can Be Downloaded To My Local. \nSend /Storageinfo To Know The Media, That Is Currently Downloaded.</b>"
-    FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia To Delete This Media, From My Storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t A Small Photo / Video, From The Above Media."
-    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "<b>A Saved Media Already Exists. Please Send /Storageinfo To Know The Current Media Details.</b>"
-    USER_DELETED_FROM_DB = "User <a href='tg://user?id={}'>{}</a> deleted from DataBase."
-    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "<b>Reply to a Telegram media (MKV), to extract embedded streams.</b>"
-    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "<b>Reply /generatecustomthumbnail to a media album, to generate custom thumbail</b>"
-    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "<b>Media Album Should Contain Only Two Photos. Please Re-send The Media Album, And Then Try Again, Or Send Only Two Photos In An Album.</b>"
-    INVALID_UPLOAD_BOT_URL_FORMAT = "<b>URL format is incorrect. make sure your url starts with either http:// or https://. You can set custom file name using the format link | file_name.extension</b>"
-    ABUSIVE_USERS = "<b>You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction.</b>"
-    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "Holy Shit!!"
-    EXTRACT_ZIP_INTRO_ONE = "Send a compressed file first, Then reply /unzip command to the file."
-    EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
-    UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
-    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice."
-    EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options.
-You can use /rename command after receiving file to rename it with custom thumbnail support."""
-    CANCEL_STR = "Process Cancelled"
-    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
-    FREE_USER_LIMIT_Q_SZE = """Cannot Process.
- users only 1 request per 5 minutes.
-/upgrade or Try 300 seconds later."""
-    SLOW_URL_DECED = "Gosh That Seems To Be A Very Slow URL. Since You Were Screwing My Home, I Am In No Mood To Download This File. Meanwhile, Why Don't You Get Me A Fast URL So That I can Upload To Telegram, Without Me Slowing Down For Other Users."
-    IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
-The given file name has {num} characters.
+<b>Download Media</b>
+- Send /downloadmedia to download Telegram media to my Server.
 
-<b>Essays Not allowed in Telegram file name!</b>
-Please short your file name and try again!"""
+<b>Get Link</b>
+- Reply to Telegram Media with /getlink to get Link
+
+<b>Convert to Video</b>
+- Reply With telegram media /converttovideo to Convert video or file
+
+<b>Rename</b>
+- Long press and Reply With telegram media /rename with Extension or new Name to rename telegram file or video
+
+<b>Convert to Audio</b>
+- Reply With telegram media /converttoaudio to Convert video or file in Audio
+
+<b>Trim</b>
+- Reply With telegram media /trim to Trim video or file
+
+<b>Storage info</b>
+- Reply with /storageinfo to see Details of Telegram downloaded media
+
+<b>Generate Custom Thumbnail</b>
+- Reply With /generatecustomthumbnail to generate custom thumbnail
+
+<b>Generate Screenshots</b>
+- Reply With telegram media /generatescss to Generate Screenshots
+
+<b>Clear FFmpeg</b>
+- Reply With /clearffmpegmedia to Clear Stored Media From FFmpeg
+
+<b>FFmpeg Robot</b>
+- Reply With /ffmpegrobot to get info about FFmpeg
+"""
+    ABOUT_TEXT = """
+🤖 Bot : URL Uploader\n
+🧒 Creator : [Vivek](https://telegram.me/vivektvp)\n
+📡 Channel : [Vk Projects](https://telegram.me/VKPROJECTS)\n
+🎉 Credits : Everyone in this journey\n
+⚗️ Language : [Python3](https://python.org)\n
+📚 Library : [Pyrogram v1.2.0](https://pyrogram.org)\n
+💫 Server : [Heroku](https://heroku.com)\n
+"""
+    START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⛔ Help', callback_data='help'),
+        InlineKeyboardButton('👲 About', callback_data='about'),
+        ],[
+        InlineKeyboardButton('Close🔐', callback_data='close')
+        ]]
+    )
+    HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('👲 About', callback_data='about'),
+        InlineKeyboardButton('Close 🔐', callback_data='close')
+        ]]
+    )
+    ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('Close 🔐', callback_data='close')
+        ]]
+    )
